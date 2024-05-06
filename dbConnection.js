fef9aa5868e2b588d38299390f2067440e3189dd
Yousuf Khan <34688697+yousufk1994@users.noreply.github.com>
2024-05-07 01:35:35 +0500

    Deployed online database and also write env file

--- dbConnection.js
+++ dbConnection.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 const db = mongoose.connection;
+
+const localDatabaseURL = process.env.dbURL_Local;
+const dbURL = process.env.dbURL
 
 db.on('connected',()=>{
     console.log("Connected to MongoDB");
 })
 
 
-module.exports = mongoose.connect('mongodb://localhost:27017/mern');
+module.exports = mongoose.connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
--- server.js
+++ server.js
@@ -5,8 +5,10 @@ app.use(bodyParser.json())
 const menuItem = require('./Controller/MenuItem')
 const EmployeeC = require('./Controller/EmployeeC')
 const dC = require('./dbConnection')
-
+require('dotenv').config()
+const PORT = process.env.PORT
 app.use('/employee',EmployeeC);
 app.use('/menuitem',menuItem);
 
-app.listen(4000,()=>{ console.log("SERVER HAS BEEN STARTED")})
+
+app.listen(PORT,()=>{ console.log("SERVER HAS BEEN STARTED")})
